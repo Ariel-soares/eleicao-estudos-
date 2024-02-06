2f5712c64b23877f1f47ca7128520339b35911bd
Ariel Soares <ariel.sfranco@gmail.com>
2024-02-06 19:52:23 -0300

    Renovação do projeto

--- src/application/App.java
+++ src/application/App.java
@@ -3,48 +3,44 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.TreeMap;
+
+import entities.Employee;
 
 public class App {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
 
-		System.out.println("Enter file full path: ");
-		String path = sc.nextLine();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			Map<String, Integer> candidates = new TreeMap<>();
-
+		System.out.print("Insira o caminho do arquivo: ");
+		String path = sc.next();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			String line = br.readLine();
-
-			while (line != null) {
-
+			
+			while(line != null) {
 				String[] fields = line.split(",");
-				String name = fields[0];
-				Integer votes = Integer.parseInt(fields[1]);
-
-				if (candidates.containsKey(name)) {
-					int VotesSoFar = candidates.get(name);
-					candidates.put(name, votes + VotesSoFar);
-				} else {
-					candidates.put(name, votes);
-				}
-				line = br.readLine();
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();			
 			}
-
-			for(String key : candidates.keySet()) {
-			System.out.println(key + ": " + candidates.get(key));
-			}
-		} catch (IOException e) {
-			System.out.println("Error " + e.getMessage());
-			;
+			
+			System.out.print("Insira o salário: ");
+			Double salary = sc.nextDouble();
+			
+			List<String> list2 = (List<String>) list.stream().map(p -> p.getName());
+			
+			
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
 		}
-
+		
 		sc.close();
 	}
 
